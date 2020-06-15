346ae2759b330d4988bbf7b772e568a4240cbdff
msbense <marksbense@gmail.com>
2020-06-15 13:57:50 -0400

    made threadpool_list_ an std::list, as ThreadPools have non-copyable/movable members (required by std::vector)

--- numa_task_scheduler.h
+++ numa_task_scheduler.h
@@ -1,27 +1,49 @@
 #pragma once
-#include <vector>
+#include <list>
 #include <functional>
 #include "logging.h"
 #include "numa.h"
 #include "threadpool.h"
 
+enum class NUMABindPolicy {
+    kNonStrict,
+    kStrict
+};
 
+//TODO memory binding policies
 class NUMATaskScheduler {
     
     public:
         NUMATaskScheduler() : num_numa_nodes_(NUMANumNodes()) {
-            std::function<void(int)> bind_to_node = [](int node) { NUMAThreadBind(node); };
+            std::function<void(int)> bind_to_node = [](int node) { 
+                NUMAThreadBind(node); 
+            };
+            
             for (int i = 0; i < num_numa_nodes_; i++) {
-                ThreadPool tp(NUMANumCoresPerNode(), std::bind(bind_to_node, i));
-                threadpool_list_.push_back(tp);
+                // ThreadPool tp(NUMANumCoresPerNode(), std::bind(bind_to_node, i));
+                threadpool_list_.emplace_back(NUMANumCoresPerNode(), std::bind(bind_to_node, i));
             } 
         }
 
-        void Schedule(std::function<void()> f, int node) {
-            
+        //false if task cannot be scheduled
+        bool Schedule(std::function<void()> f, int node, NUMABindPolicy bind_pref) {
+            if (node < 0 || threadpool_list_.size() < node) {
+                return false;
+            }
+            std::list<ThreadPool>::iterator i = threadpool_list_.begin();
+            if (node > 0) std::advance(i, node); 
+            if (bind_pref == NUMABindPolicy::kStrict) {
+                (*i).Schedule(f);
+            }
+            else if (bind_pref == NUMABindPolicy::kNonStrict) {
+                if ((*i).NumInactiveThreads > 0) {
+                    
+                }
+            }   
+            return true;
         }
 
     private:
-        std::vector<std::reference_wrapper<ThreadPool>> threadpool_list_;
+        std::list<ThreadPool> threadpool_list_;
         int num_numa_nodes_;
 };
